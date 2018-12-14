6a93e3e87ae66909595f5e11f8618201b95d9178
Maarten van Ee <maartenvanee@gmail.com>
2018-12-14 10:23:41 +0100

    graphql apollo

--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ import Main from './pages/Main'
 
 Vue.config.productionTip = false
 
+const apolloProvider = new VueApollo.ApolloProvider({
+  // defaultClient: url_here,
+})
+
+const POSTS_QUERY = Apollo.gql `
+{
+  movies {
+    id
+    title
+  }
+}`
+
 new Vue({
   render: h => h(Main),
+
+  // Apollo GraphQL
+  apolloProvider,
+  apollo: {
+    posts: {
+      query: POSTS_QUERY,
+      loadingKey: 'loading',
+    },
+  },
 }).$mount('#app')
